d6135db77a8ce4088081f3ee5f9110d6b3b11a23
pr3b <eb.underwood22@gmail.com>
2023-06-23 00:29:22 +0700

    mindkey-test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,9 @@ const config = {
   url: "https://pr3b.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/mindkey-2/",
+  baseUrl: "/",
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
